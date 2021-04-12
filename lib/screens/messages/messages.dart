e76a3207bdb34a574e32f9b85120e1271ff7e2b1
Elf <a.blake.mail@gmail.com>
2021-04-12 15:17:00 +0300

    v0.08

--- lib/screens/messages/messages.dart
+++ lib/screens/messages/messages.dart
@@ -20,22 +20,62 @@ class Messages extends StatelessWidget {
             onChanged: (value) {},
           ),
         ),
-        SizedBox(
-          height: 10,
-        ),
-        ChatRow(
-          imgURL: 'assets/image/chatAvatars/till.jpg',
-          name: 'Till',
-          msgPreview: 'Did you like the latest album?mmmmmmmm',
-          msgCount: 4,
-          time: '14:16',
-        ),
-        ChatRow(
-          imgURL: 'assets/image/chatAvatars/matt.jpg',
-          name: 'Matt Wellamy',
-          msgPreview: 'I like the vibes!',
-          msgCount: 11,
-          time: '10:11',
+        Expanded(
+          child: ListView(
+            children: [
+              ChatRow(
+                imgURL: 'assets/image/chatAvatars/till.jpg',
+                name: 'Till',
+                msgPreview: 'Did you like the latest album?mmmmmmmm',
+                msgCount: 4,
+                time: '14:16',
+                //isOnline: true,
+              ),
+              ChatRow(
+                imgURL: 'assets/image/chatAvatars/matt.jpg',
+                name: 'Matt Wellamy',
+                msgPreview: 'I like the vibes!',
+                msgCount: 11,
+                time: '10:11',
+              ),
+              ChatRow(
+                imgURL: 'assets/image/chatAvatars/lana.jpg',
+                name: 'Lana Del Ring',
+                msgPreview: 'Summertime finally!',
+                msgCount: 9,
+                time: '9:23',
+              ),
+              ChatRow(
+                imgURL: 'assets/image/chatAvatars/tom.jpg',
+                name: 'Tom Angeles',
+                msgPreview: 'This gonna be a hit',
+                //msgCount: 9,
+                time: '7:00',
+                isOnline: true,
+              ),
+              ChatRow(
+                imgURL: 'assets/image/chatAvatars/tobias.jpg',
+                name: 'Tobias Mold',
+                msgPreview: 'Trying my best!',
+                //msgCount: 9,
+                time: '6:55',
+              ),
+              ChatRow(
+                imgURL: 'assets/image/chatAvatars/tobias.jpg',
+                name: 'Tobias Mold',
+                msgPreview: 'Trying my best!',
+                //msgCount: 9,
+                time: '6:55',
+              ),
+              ChatRow(
+                imgURL: 'assets/image/chatAvatars/tobias.jpg',
+                name: 'Tobias Mold',
+                msgPreview: 'Trying my best!',
+                //msgCount: 9,
+                time: '6:55',
+              ),
+            ],
+          ),
         ),
       ],
     );
@@ -43,76 +83,118 @@ class Messages extends StatelessWidget {
 }
 
 class ChatRow extends StatelessWidget {
-  ChatRow({this.imgURL, this.name, this.msgPreview, this.msgCount, this.time});
+  ChatRow(
+      {this.imgURL,
+      this.name,
+      this.msgPreview,
+      this.msgCount,
+      this.time,
+      this.isOnline});
 
   final String imgURL, name, msgPreview, time;
   final int msgCount;
+  final bool isOnline;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+    return InkWell(
+      onTap: () {
+        print('chat row tapped');
+      },
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10),
-        child: Row(
-          children: [
-            CircleAvatar(
-              radius: 30,
-              backgroundColor: kBottomMenuBG,
-              child: CircleAvatar(
-                radius: 27,
-                backgroundImage: AssetImage(imgURL),
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Stack(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: kBottomMenuBG,
+                          child: CircleAvatar(
+                            radius: 27,
+                            backgroundImage: AssetImage(imgURL),
+                          ),
+                        ),
+                        Positioned(
+                          right: 3,
+                          top: 3,
+                          child: Container(
+                            width: 12,
+                            height: 12,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: (isOnline == true)
+                                  ? Color(0xFF5DD27E)
+                                  : Colors.transparent,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 11,
+                    ),
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            name,
+                            style: TextStyle(fontSize: 20, color: kMainWhite),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            msgPreview,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: TextStyle(
+                                color: kSecondaryColor,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              width: 11,
-            ),
-            Flexible(
-              flex: 2,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
                 children: [
-                  Text(
-                    name,
-                    style: TextStyle(fontSize: 20, color: kMainWhite),
+                  Badge(
+                    showBadge: (msgCount != null) ? true : false,
+                    shape: BadgeShape.square,
+                    toAnimate: false,
+                    badgeColor: kThemeColor,
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                    borderRadius: BorderRadius.circular(50),
+                    badgeContent: Text('$msgCount',
+                        style: TextStyle(
+                            color: kMainWhite,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12)),
                   ),
                   SizedBox(
-                    height: 5,
+                    width: 14,
                   ),
-                  Text(
-                    msgPreview,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                    style: TextStyle(
-                        color: kSecondaryColor,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(minWidth: 40),
+                    child: Text(
+                      time,
+                      style: TextStyle(
+                          color: kSecondaryColor, fontWeight: FontWeight.w500),
+                    ),
                   ),
                 ],
               ),
-            ),
-            //Spacer(),
-            Badge(
-              shape: BadgeShape.square,
-              toAnimate: false,
-              badgeColor: kThemeColor,
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
-              borderRadius: BorderRadius.circular(50),
-              badgeContent: Text('$msgCount',
-                  style: TextStyle(
-                      color: kMainWhite,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 12)),
-            ),
-            SizedBox(
-              width: 14,
-            ),
-            Text(
-              time,
-              style: TextStyle(
-                  color: kSecondaryColor, fontWeight: FontWeight.w500),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/search/search.dart
+++ lib/screens/search/search.dart
@@ -5,149 +5,155 @@ import 'package:lofi/constants.dart';
 class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            padding: EdgeInsets.only(top: 50, left: 16),
-            width: double.infinity,
-            color: kBottomMenuBG,
-            child: TextField(
-              style: TextStyle(
-                color: kMainWhite,
-              ),
-              decoration: kTextFieldInputDecoration.copyWith(
-                  hintText: 'Find artist, album, song'),
-              onChanged: (value) {},
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: 50, left: 16),
+          width: double.infinity,
+          color: kBottomMenuBG,
+          child: TextField(
+            style: TextStyle(
+              color: kMainWhite,
             ),
+            decoration: kTextFieldInputDecoration.copyWith(
+                hintText: 'Find artist, album, song'),
+            onChanged: (value) {},
           ),
-          SizedBox(
-            height: 58,
-          ),
-          GestureDetector(
-            onTap: () {
-              print('whats playing was tapped');
-            },
-            child: Chip(
-              avatar: SvgPicture.asset('assets/icons/whatsPlaying.svg'),
-              backgroundColor: kMainWhite,
-              padding: EdgeInsets.all(12),
-              label: Text(
-                'What\'s playing?',
-                style: TextStyle(
-                    color: kBgColor, fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        Expanded(
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 32,
               ),
-            ),
-          ),
-          SizedBox(
-            height: 58,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              //mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  'Recent searches',
-                  style: TextStyle(
-                    color: kMainWhite,
-                    fontSize: 22,
-                    fontWeight: FontWeight.w500,
+              GestureDetector(
+                onTap: () {
+                  print('whats playing was tapped');
+                },
+                child: Chip(
+                  avatar: SvgPicture.asset('assets/icons/whatsPlaying.svg'),
+                  backgroundColor: kMainWhite,
+                  padding: EdgeInsets.all(12),
+                  label: Text(
+                    'What\'s playing?',
+                    style: TextStyle(
+                        color: kBgColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 26),
-                  child: InkWell(
-                    onTap: () {
-                      print('song was tapped');
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          //картинка и надписи
+              ),
+              SizedBox(
+                height: 58,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  //mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Recent searches',
+                      style: TextStyle(
+                        color: kMainWhite,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 26),
+                      child: InkWell(
+                        onTap: () {
+                          print('song was tapped');
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Container(
-                              child: ClipRRect(
-                                borderRadius:
-                                    BorderRadius.circular(kBorderRadiusDefault),
-                                child: Image.asset(
-                                  'assets/image/woodkid.jpg',
-                                  height: 48,
-                                  width: 48,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                            Row(
+                              //картинка и надписи
                               children: [
-                                Text(
-                                  'Iron',
-                                  style: TextStyle(
-                                      color: kMainWhite,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500),
+                                Container(
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(
+                                        kBorderRadiusDefault),
+                                    child: Image.asset(
+                                      'assets/image/woodkid.jpg',
+                                      height: 48,
+                                      width: 48,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
                                 ),
                                 SizedBox(
-                                  height: 4,
+                                  width: 10,
                                 ),
-                                Text(
-                                  'Woodkid',
-                                  style: TextStyle(
-                                      color: kSecondaryColor,
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w500),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Iron',
+                                      style: TextStyle(
+                                          color: kMainWhite,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    SizedBox(
+                                      height: 4,
+                                    ),
+                                    Text(
+                                      'Woodkid',
+                                      style: TextStyle(
+                                          color: kSecondaryColor,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
+                            IconButton(
+                                icon: SvgPicture.asset('assets/icons/more.svg'),
+                                onPressed: () {
+                                  print('more button was tapped');
+                                }),
                           ],
                         ),
-                        IconButton(
-                            icon: SvgPicture.asset('assets/icons/more.svg'),
-                            onPressed: () {
-                              print('more button was tapped');
-                            }),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 21,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Wrap(
+                  spacing: 16,
+                  runSpacing: 16,
+                  children: [
+                    Tag('Blues'),
+                    Tag('Business'),
+                    Tag('Brazilian'),
+                    Tag('Chill'),
+                    Tag('Classical'),
+                    Tag('Country'),
+                    Tag('EDM'),
+                    Tag('Education'),
+                    Tag('Electronic'),
+                    Tag('Folk'),
+                    Tag('Gaming'),
+                    Tag('Indie'),
+                    Tag('Jazz'),
+                    Tag('K-pop'),
+                  ],
+                ),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 21,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Wrap(
-              spacing: 16,
-              runSpacing: 16,
-              children: [
-                Tag('Blues'),
-                Tag('Business'),
-                Tag('Brazilian'),
-                Tag('Chill'),
-                Tag('Classical'),
-                Tag('Country'),
-                Tag('EDM'),
-                Tag('Education'),
-                Tag('Electronic'),
-                Tag('Folk'),
-                Tag('Gaming'),
-                Tag('Indie'),
-                Tag('Jazz'),
-                Tag('K-pop'),
-              ],
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
